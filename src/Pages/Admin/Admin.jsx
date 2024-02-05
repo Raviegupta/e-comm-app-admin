0a373765b775936e72b2f5496a9d5db07108b423
Raviegupta <raviegupta153@gmail.com>
2024-02-05 20:28:55 +0530

    react app setup using vite for adminpanel

---
+++ src/Pages/Admin/Admin.jsx
@@ -0,0 +1,12 @@
+import Navbar from '../../Components/Navbar/Navbar'
+import './Admin.css'
+
+const Admin = () => {
+  return (
+    <div className='admin'>
+        <Navbar />
+    </div>
+  )
+}
+
+export default Admin
