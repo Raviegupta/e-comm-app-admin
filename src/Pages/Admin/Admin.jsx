c77a7cf53e484a93f89e0e5e6c583ec43d3a04a9
Raviegupta <raviegupta153@gmail.com>
2024-02-06 08:38:12 +0530

    Admin Compo, Sidebar Compo, AddProduct Compo, hitting upload & addproduct endpoints

--- src/Pages/Admin/Admin.jsx
+++ src/Pages/Admin/Admin.jsx
@@ -1,10 +1,19 @@
-import Navbar from '../../Components/Navbar/Navbar'
+import AddProduct from '../../Components/AddProduct/AddProduct'
+import ListProduct from '../../Components/ListProduct/ListProduct'
+import Sidebar from '../../Components/Sidebar/Sidebar'
+import { Routes, Route } from 'react-router-dom'
 import './Admin.css'
 
 const Admin = () => {
   return (
     <div className='admin'>
-        <Navbar />
+        <Sidebar />
+        <AddProduct />
+        <ListProduct />
+        <Routes>
+          <Route path='/addproduct' element={<AddProduct />} />
+          <Route path='/listproduct' element={<ListProduct />} />
+        </Routes>
     </div>
   )
 }
